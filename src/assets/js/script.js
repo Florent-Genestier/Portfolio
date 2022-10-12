b090260fa22c3f941511a009dc9813c1b8fce9da
Florent-Genestier <florentgenestier@gmail.com>
2022-10-12 16:11:19 +0200

    initial project version

---
+++ src/assets/js/script.js
@@ -0,0 +1,7 @@
+/* *********** TYPING ANIMATION ****************************** */
+var typed = new Typed(".typing", {
+    strings:["Florent Genestier","Back-End Developer"],
+    typeSpeed:100,
+    BackSpeed:90,
+    loop:true
+})
