32936710506b3af40c426c26d5b2aa0179efbdfa
Florent-Genestier <florentgenestier@gmail.com>
2022-11-17 13:29:47 +0100

    css comment maj

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,6 +1,3 @@
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
-
-
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
