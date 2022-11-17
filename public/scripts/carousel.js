d5cd96d9a2c2cfb4afe40440ed4eacd334400fca
Florent-Genestier <florentgenestier@gmail.com>
2022-11-17 09:38:15 +0100

    responsive aspect

---
+++ public/scripts/carousel.js
@@ -0,0 +1,16 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+
+
+
+var swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  
