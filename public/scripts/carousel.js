cccd57d46aab473d45aaa257bf1ea10ea903308f
Florent-Genestier <florentgenestier@gmail.com>
2022-11-17 12:24:16 +0100

    carousel

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,16 +1,15 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 
-
-var swiper = new Swiper('.swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
   
   
